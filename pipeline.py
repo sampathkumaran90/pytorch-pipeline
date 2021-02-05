ee6ca016c81bc0983ba136c28d2739c09cbafc34
Pavel Dournov <dournov@google.com>
2021-02-04 17:09:38 -0800

    minor fixes, remove separate kfp script

--- pipeline.py
+++ pipeline.py
@@ -43,9 +43,6 @@ def train_imagenet_cnn_pytorch(
     
     data_prep_task = data_prep_op(region = 'us-central1', input_data = training_data_path)
 
-    #test_training_data_location = data_prep_task.outputs["output_data"]
-    #test_training_data_location = "gs://managed-pipeline-test-bugbash/20210130/pipeline_root/pavel/186556260430/us-central1/pytorchcnn-20210131142111/PreProcessImageData/output_data"
-
     train_model_task = (train_model_op(data_prep_task.outputs["output_data"]).
         set_cpu_limit('4').
         set_memory_limit('14Gi').
